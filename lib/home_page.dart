c8061d6dff9675d5e7845a31cb3ddcd65c8c6e22
junai <junaidchohan@gmail.com>
2023-03-25 10:16:43 +0500

    CATALOG APP

---
+++ lib/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  int days = 30;
+  String name = 'junaid chohan';
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+        child: Container(
+          child: Text('well come to $days day $name'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+int days = 30;
+String name = 'junaid ';
+
+@override
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
